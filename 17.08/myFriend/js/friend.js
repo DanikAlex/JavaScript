05b8c71e73003eaae7ba3f46ef182a87d63b4d5c
DanikAlex <danik20021@gmail.com>
2023-08-17 21:00:40 +0300

    friend list

--- 17.08/myFriend/js/friend.js
+++ 17.08/myFriend/js/friend.js
@@ -1,4 +1,7 @@
 function loadImage(){
-    img = document.getElementById("imageUrl").value;
-    document.getElementById("friendImage").src=img;
+    var img = document.getElementById("imageUrl").value;
+    var friendName=document.getElementById("friendName").value;
+    var friendTel=document.getElementById("friendTel").value;
+    // document.getElementById("friendImage").src=img;
+    document.getElementById("friendList").innerHTML=`<tr><td><img src="${img}" width="50"></td><td>${friendName}</td><td>${friendTel}</td></tr>` 
 }
