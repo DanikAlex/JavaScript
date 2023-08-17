2016635d61b191356d597f36d3b8c3aa2a617d6c
DanikAlex <danik20021@gmail.com>
2023-08-17 20:46:16 +0300

    load image

--- 17.08/login-assigment/js/myjs.js
+++ 17.08/login-assigment/js/myjs.js
@@ -1,10 +1,9 @@
+const uName="daniel";
+const uPass="12345";
+
 function checkLogin(){
     var userName=document.getElementById("userName").value;
     var userPass=document.getElementById("userPass").value;
-    if (userName!="Daniel" && userPass!="12345") {
-        document.getElementById("result").innerText="Try again";
-    } else {
-        document.getElementById("result").innerText="login successful";
-    }
+    document.getElementById("result").innerHTML = userName!=uName && userPass!=uPass?"wrong identification":"login successful";
 }
 
---
+++ 17.08/myFriend/js/friend.js
@@ -0,0 +1,4 @@
+function loadImage(){
+    img = document.getElementById("imageUrl").value;
+    document.getElementById("friendImage").src=img;
+}
