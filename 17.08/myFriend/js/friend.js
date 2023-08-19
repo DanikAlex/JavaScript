8472b01481cbe30301469c561ef08626665ac48e
DanikAlex <danik20021@gmail.com>
2023-08-19 14:54:36 +0300

    homework with js explanation

--- 17.08/myFriend/js/friend.js
+++ 17.08/myFriend/js/friend.js
@@ -3,13 +3,16 @@ function loadImage(){
     var friendName=document.getElementById("friendName").value;
     var friendTel=document.getElementById("friendTel").value;
     // document.getElementById("friendImage").src=img;
-    var newRow = `
+        // added a new variable with the body of the table so i would have more than 1 row to merge
+    var newRow = ` 
         <tr>
             <td><img src="${img}" width="50"></td>
             <td>${friendName}</td>
             <td>${friendTel}</td>
         </tr>    
     `;
+    // added a new variable called friendList for the tbody id
     var friendList = document.getElementById("friendList");
+    // merged the newRow variable into the friendList variable to extent the table each time for a countless amount of time
     friendList.innerHTML += newRow;
 }
