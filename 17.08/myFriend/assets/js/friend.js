8df29f18506fed8ae39350203af40287432d689e
DanikAlex <danik20021@gmail.com>
2023-08-21 18:19:23 +0300

    js+css

--- 17.08/myFriend/assets/js/friend.js
+++ 17.08/myFriend/assets/js/friend.js
@@ -1,7 +1,11 @@
+var imageUrl = [];
+var friendTel = [];
+var friendName = [];
+
 function loadImage(){
-    var imageUrl = document.getElementById("imageUrl").value;      //input
-    var friendTel = document.getElementById("friendTel").value;    //input
-    var friendName = document.getElementById("friendName").value;  //input
+    var urlData = document.getElementById("imageUrl").value;      //input
+    var telData = document.getElementById("friendTel").value;    //input
+    var nameData = document.getElementById("friendName").value;  //input
 
     var data = document.getElementById("data");                    //output
 
