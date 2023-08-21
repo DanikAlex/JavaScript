a85f7437a90a9bff4a612cc1da58391c39b2563a
DanikAlex <danik20021@gmail.com>
2023-08-21 20:51:45 +0300

    save and load data

--- 17.08/myFriend/assets/js/friendObject.js
+++ 17.08/myFriend/assets/js/friendObject.js
@@ -1,4 +1,5 @@
 var friendList=[];
+loadData();
 
 function loadImage(){
     var urlData = document.getElementById("imageUrl").value;      //input
@@ -13,7 +14,7 @@ function loadImage(){
     friendList.push(data);
 
     createTable();
-
+    saveData();
     document.getElementById("friendForm").reset();  
 }
 
@@ -46,3 +47,19 @@ function removeLast(){
     friendList.pop();
     createTable();
 }
+
+function loadData(){
+    //localStorage.getItem get data from local storage by name
+    //JSON.parse => convert the string (in json format to object again)
+    friendList = JSON.parse(localStorage.getItem("friends")); 
+    createTable();
+}
+
+function saveData(){
+    //localStorage => save data to user hard disk
+    //sessionStorage => save data as long as the window/tab is open
+    //localStorage.setItem => save the data to specific item...
+    localStorage.setItem("friends",JSON.stringify(friendList));
+    //the command to convert an object to a string is JSON.stringify(object)
+}
+
