f184705b6be2255fc7c9a7d0df4995882ba62ed7
DanikAlex <danik20021@gmail.com>
2023-09-28 22:38:49 +0300

    4

--- 28.09/class assigment/assets/js/main.js
+++ 28.09/class assigment/assets/js/main.js
@@ -1,11 +1,16 @@
 const displayLocation = () => {
-    getUserLocation(location => console.log(`${location.latitude}, ${location.longitude}`), error => console.log(`error is ${error}`));
-}
+  getUserLocation(
+    (location) => console.log(`${location.latitude}, ${location.longitude}`),
+    (error) => console.log(`error is ${error}`)
+  );
+};
 
 const getUserLocation = (successCallback, errorCallback) => {
-    try {
-        navigator.geolocation.getCurrentPosition(position => successCallback(position.coords));
-    } catch (err) {
-        errorCallback(err);
-    }    
-}
+  try {
+    navigator.geolocation.getCurrentPosition((position) =>
+      successCallback(position.coords)
+    );
+  } catch (err) {
+    errorCallback(err);
+  }
+};
