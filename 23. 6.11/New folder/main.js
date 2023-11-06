87a84dadcf70071a5d6b30c728299f57a8ca7e6c
Daniel Alexandrov <danik20021@gmail.com>
2023-11-06 20:40:28 +0200

    recursion array max

--- 23. 6.11/New folder/main.js
+++ 23. 6.11/New folder/main.js
@@ -33,3 +33,23 @@ function factorial(n){
     if (n <= 0) return 1;
     return n * factorial(n-1);
 }
+
+function displayArrayMax(){
+    const arr = [2, 4, 6, 8, 10, 12];
+    const max = recursiveArrayMax(arr, 0, 5);
+    console.log(max);
+}
+
+function recursiveArrayMax(arr, startIndex, endIndex){
+    if (startIndex === endIndex) return arr[startIndex];
+    const max = recursiveArrayMax(arr, startIndex + 1, endIndex);
+    if (arr[startIndex] > max) {
+        return arr[startIndex];
+    }
+    return max;
+}
+
+// function recursiveArrayMax2(arr, startIndex, endIndex){
+//     if (startIndex === endIndex) return arr[startIndex];
+//     return arr[startIndex] > arr[endIndex] ? recursiveArrayMax2(arr, startIndex, endIndex - 1) : recursiveArrayMax2(arr, startIndex - 1, endIndex);
+// } 
