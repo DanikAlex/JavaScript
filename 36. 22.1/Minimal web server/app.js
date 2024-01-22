9e7ee15aba375f8a42e4f644aa3120fc8a3993b0
Daniel Alexandrov <danik20021@gmail.com>
2024-01-22 21:22:31 +0200

    minimal server

--- 36. 22.1/Minimal web server/app.js
+++ 36. 22.1/Minimal web server/app.js
@@ -1,6 +1,13 @@
 require("http")
   .createServer((request, response) => {
-    response.writeHead(200).end("hello from your minimal web server");
+    switch (request.url) {
+        case '/':
+            response.writeHead(200).end('Homepage');
+            break;
+        case '/products':
+            response.writeHead(200).end('Products');
+            break;
+    }
   })
   .listen(8080), () => {
     console.log(' web server started on http://localhost:8080')
